70c3db19baf9999ed94b091f1215f0be3d50aecd
John Grib <johngrib@woowahan.com>
2017-11-06 21:16:28 +0900

    ReadingListRepository: 조회 메서드 파라미터 변경

--- src/main/java/readinglist/ReadingListRepository.java
+++ src/main/java/readinglist/ReadingListRepository.java
@@ -1,11 +1,13 @@
 package readinglist;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
 
-    List<Book> findByReader(String reader);
+    List<Book> findByReader(Reader reader);
 
 }
